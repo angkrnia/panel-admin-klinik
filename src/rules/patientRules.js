14b24569689948c33aa1d4033932147995d09440
angkrnia <anggakurnia712@gmail.com>
2024-11-30 19:53:04 +0700

    rule

--- src/rules/patientRules.js
+++ src/rules/patientRules.js
@@ -1,11 +1,11 @@
 import { emailRule, maxStringRule, requiredRule } from ".";
 
 export const patientRule = {
-  fullname: [requiredRule(), maxStringRule(50)],
+  fullname: [requiredRule(), maxStringRule(255)],
   gender: [requiredRule()],
   no_ktp: [maxStringRule(16)],
   phone: [requiredRule(), maxStringRule(20)],
-  nama_keluarga: [requiredRule(), maxStringRule(16)],
+  nama_keluarga: [requiredRule(), maxStringRule(255)],
   birthday: [maxStringRule(20)],
   age: [requiredRule()],
   address: [requiredRule(), maxStringRule(150)],
